08acb63de3c978b8b5498a41b3ed664b2902578c
Don Lokuge <donlokuge@gmail.com>
2025-03-19 22:34:59 +1300

    Update build command for ARM64 architecture and clean up hello_world_handler

--- apps/rust_lambda/src/handlers/hello_world_handler.rs
+++ apps/rust_lambda/src/handlers/hello_world_handler.rs
@@ -1,4 +1,4 @@
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
@@ -10,21 +10,15 @@ struct Request {
 #[derive(Serialize)]
 struct Response {
     message: String,
-    request_id: String,
 }
 
 async fn function_handler(event: LambdaEvent<Request>) -> Result<Value, Error> {
-    // Extract some useful info from the request
     let name = event.payload.name;
-    let request_id = event.context.request_id;
 
-    // Prepare the response
     let resp = Response {
         message: format!("Hello, {}!", name),
-        request_id,
     };
 
-    // Return a response as a JSON object
     Ok(json!(resp))
 }
 
